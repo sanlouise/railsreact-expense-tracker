d504e9a8677d07ae9113af09b671b1a1a697f29d
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-17 09:39:22 +0200

    Update AmountBox Component to ES2015 syntax

--- app/assets/javascripts/components/amount_box.js
+++ app/assets/javascripts/components/amount_box.js
@@ -1,9 +1,9 @@
 this.AmountBox = React.createClass({
-  render: function() {
+  render() {
     return React.DOM.div({
       className: 'col-md-4'
     }, React.DOM.div({
-      className: "panel panel-" + this.props.type
+      className: `panel panel-${this.props.type}`
     }, React.DOM.div({
       className: 'panel-heading'
     }, this.props.text), React.DOM.div({
